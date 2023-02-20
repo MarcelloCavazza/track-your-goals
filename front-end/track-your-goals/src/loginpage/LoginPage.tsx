1b10596d289404cf23b7c40809118c261c766666
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-19 21:14:40 -0300

    removing repeated structure

--- front-end/track-your-goals/src/loginpage/LoginPage.tsx
+++
@@ -1,9 +0,0 @@
-import FormsStructure from "./FormsStructure";
-
-function LoginPage() {
-    return (
-        <FormsStructure display="Login" />
-    )
-}
-
-export default LoginPage;
--- front-end/track-your-goals/src/loginpage/SingUpPage.tsx
+++
@@ -1,9 +0,0 @@
-import FormsStructure from "./FormsStructure";
-
-function SingUpPage() {
-    return (
-        <FormsStructure display="Sign Up" />
-    )
-}
-
-export default SingUpPage;
