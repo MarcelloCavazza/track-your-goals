e083c8da9f7ed8502d4f968a8b480eb12900f107
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-19 21:16:26 -0300

    will be created

---
+++ front-end/track-your-goals/src/DoneGoals/DoneGoals.tsx
@@ -0,0 +1,9 @@
+export default function DoneGoals() {
+    return (
+        <div>
+            <div>
+                {/* will be the list of achieved goals */}
+            </div>
+        </div>
+    )
+}
