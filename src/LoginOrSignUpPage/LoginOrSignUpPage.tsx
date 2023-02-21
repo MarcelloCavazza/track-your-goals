0af4206b0d1990680649fe702a6caa8914acc108
Marcello Cavazza <63473199+MarcelloCavazza@users.noreply.github.com>
2023-02-20 22:51:49 -0300

    moving front-end to root folder

---
+++ src/LoginOrSignUpPage/LoginOrSignUpPage.tsx
@@ -0,0 +1,9 @@
+import FormsStructure from "../formstructure/FormsStructure";
+
+function LoginOrSignUpPage() {
+    return (
+        <FormsStructure />
+    )
+}
+
+export default LoginOrSignUpPage;
